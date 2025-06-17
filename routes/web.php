1fdbfd11a2867d7e89d79e83a2c0b3aa84bd7d5e
Daniel Chigozie <danielobichere@gmail.com>
2025-06-17 15:34:29 +0100

    added storage:link

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/show-log', function () {
+    return response()->file(storage_path('logs/laravel.log'));
+});
